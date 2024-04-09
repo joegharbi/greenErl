092fd2a7928a4e8e6e972997760938c27dea7318
Youssef Gharbi <joe.gharbi@gmail.com>
2024-04-10 00:06:44 +0200

    added screenshots.

--- ene.py
+++ ene.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import os
 
-file_path = 'C:/PhD/greenErl/measurements/higher_order_functions/results/youssef_filter/filter_filter_named.csv'
+file_path = 'C:\\PhD\\greenErl\\measurements\\raw24\\del\\map_delete_remove.csv'
 # Assuming the data is in a CSV file named 'filter_filter_fun.csv'
 data = pd.read_csv(file_path, sep=';', header=None, names=['col1', 'col2', 'col3', 'col4', 'col5', 'col6'])
 
